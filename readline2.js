5d1603c1d8e317b047e751d6d1c0f887828391c1
Riandra Alkautsar <63386068+RiandraAlkautsar@users.noreply.github.com>
2020-04-10 12:03:51 +0700

    Add files via upload

---
+++ readline2.js
@@ -0,0 +1,16 @@
+var readline = require('readline-sync');
+
+var bulat = readline.question("Masukkan bilangan bulat: ");
+var riil = readline.question("Masukkan bilangan riil:");
+
+console.log ("\n")
+
+console.log("Sebelum konversi tipe data");
+var TipeA = readline.question("tipe a: ");
+console.log("tipe b: " + TipeA );
+
+console.log ("\n")
+
+console.log("Setelah konversi tipe data");
+var TipeC = readline.question("tipe a: ");
+console.log("tipe b: " + TipeC);
